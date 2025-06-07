794b7ee3e5fb0b8bf33043a9d6a88f99808935a6
Mycloudlive20 <150905470+Mycloudlive20@users.noreply.github.com>
2025-06-07 23:13:29 +0000

    fix

--- backend/get_data.php
+++ backend/get_data.php
@@ -1,9 +1,9 @@
 <?php
-header("Access-Control-Allow-Origin: https://cbs.pfadfinder-kissing.de"); // Nur diese Domain erlauben
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS"); // Erlaubte Methoden
-header("Access-Control-Allow-Headers: Content-Type"); // Erlaubte Header
+header("Access-Control-Allow-Origin: https://cbs.pfadfinder-kissing.de");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Content-Type: application/json; charset=utf-8");
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    // Behandelt Preflight-Anfragen
     http_response_code(200);
     exit;
 }
@@ -11,16 +11,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 include("mysql_con.php");
 
 $sql = "SELECT * FROM anmeldungen ORDER BY strasse ASC";
-
-$query = mysqli_query($conn, $sql);
+$result = mysqli_query($conn, $sql);
 $output = [];
 
-while ($row = $query->fetch_assoc()) {
+while ($row = mysqli_fetch_assoc($result)) {
     $output[] = $row;
 }
-$conn->close();
 
-// JSON-Ausgabe
-header('Content-Type: application/json; charset=utf-8');
+$conn->close();
 echo json_encode($output, JSON_UNESCAPED_UNICODE);
 ?>
