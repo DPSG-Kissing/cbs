5abe379593599a05e01dfc867d77051cd0f602fc
Mycloudlive20 <150905470+Mycloudlive20@users.noreply.github.com>
2025-06-08 16:31:19 +0000

    Chat Implementierung

---
+++ chat-server.php
@@ -0,0 +1,22 @@
+<?php
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+use Cbs\ChatServer;
+
+require dirname(__FILE__) . '/vendor/autoload.php';
+require dirname(__FILE__) . '/backend/ChatServer.php';
+
+// Der Haupt-WebSocket-Server für die Echtzeit-Kommunikation
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new ChatServer()
+        )
+    ),
+    8080 // Port für die WebSocket-Verbindungen
+);
+
+echo "WebSocket-Server läuft auf Port 8080\n";
+$server->run();
+?>
